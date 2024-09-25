f0e080b4f25683389c4cbe5d9c5a566b8d609b45
Derick <derickjohny456@gmail.com>
2024-09-25 11:02:25 +0530

    third commit

--- src/components/navbarpg.jsx
+++ src/components/navbarpg.jsx
@@ -1,33 +1,57 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Register from './register';
 
+
 function Navbarpg() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-        <Navbar.Brand >
-          <h1>Visit Your Places!</h1>
-        </Navbar.Brand>
-          <Navbar.Brand href="/">Home</Navbar.Brand>
-          <Nav className="me-auto">
-          <Nav.Link onClick={handleShow}>Register</Nav.Link>
-            <Nav.Link href="/Aboutpage">About us</Nav.Link>
-            <Nav.Link href="#features">Contact us</Nav.Link>
+    <Navbar expand="lg" className="bg-success">
+      <Container fluid>
+        {/* <Navbar.Brand href="#"><h3>Visit Your Places!</h3></Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="/" className='fw-bold text-white'>Home</Nav.Link>
+            <Nav.Link onClick={handleShow} className='fw-bold text-white'>Register</Nav.Link>
+            <Nav.Link href="/Aboutpage" className='fw-bold text-white'>About us</Nav.Link>
+            <NavDropdown title="Link"  id="navbarScrollingDropdown" >
+              <NavDropdown.Item href="#action3" >Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
           </Nav>
-        </Container>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-info">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
 
-        <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Register Here !</Modal.Title>
         </Modal.Header>
@@ -45,8 +69,8 @@ function Navbarpg() {
       </Modal>
 
       </Navbar>
-    </>
+
   );
 }
 
-export default Navbarpg;
+export default Navbarpg
